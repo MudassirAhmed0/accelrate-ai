9d98e7c1bc64f942d931e3e24bd99772fee659eb
Itz-zafeer <117304007+Itz-zafeer@users.noreply.github.com>
2025-02-12 02:47:58 +0500

    origin

--- src/component/layout/header/dropdown/DropDown.js
+++ src/component/layout/header/dropdown/DropDown.js
@@ -10,11 +10,11 @@ const DropDown = ({ headerDropDown, toggleMenu }) => {
       className="opacity-0 lg:pointer-events-none group-hover/dropDown:lg:opacity-[1] group-hover/dropDown:lg:pointer-events-auto transition-all duration-500 lg:duration-0 lg:transition-none max-h-0 lg:max-h-[unset] overflow-hidden lg:overflow-visible lg:absolute inset-0 size-full bg-transparent lg:bg-[#1C1C1C] lg:pt-[5.20833333333vw] flex flex-col"
     >
       <div className="bg-transparent flex lg:bg-[#1C1C1C] wrapper lg:mt-[unset] sm:mt-[30px] mt-[20px]">
-        <div className="flex lg:flex-row flex-col flex-wrap items-stretch lg:gap-[4.16666666667vw] sm:gap-[24px] gap-[20px] border-t border-[#4B4B4B] lg:mx-[40px] lg:px-[unset] px-[2.79069767442vw] poppins lg:py-[2.5vw] sm:py-[30px] py-[20px]">
+        <div className="w-full flex lg:flex-row flex-col flex-wrap items-stretch lg:gap-[2.5vw] sm:gap-[24px] gap-[20px] border-t border-[#4B4B4B] lg:mx-[40px] lg:px-[unset] px-[2.79069767442vw] poppins lg:py-[2.5vw] sm:py-[30px] py-[20px]">
           <span className="text-[#A4A4A4] lg:mt-[0.3125vw] mtext16 hidden lg:block">
             Our services
           </span>
-          <div className="flex flex-col lg:gap-y-[unset] sm:gap-y-[15px] gap-y-[10px] justify-start lg:text35 sm:mtext28 mtext22 lg:leading-[3.125vw] tracking-tight w-full lg:w-[40%]">
+          <div className="flex flex-col lg:gap-y-[unset] sm:gap-y-[15px] gap-y-[10px] justify-start lg:text35 sm:mtext28 mtext22 lg:leading-[3.125vw] tracking-tight w-full lg:w-[41%]">
             <Link
               onClick={toggleMenu}
               href={"/service-singlepage"}
@@ -38,7 +38,7 @@ const DropDown = ({ headerDropDown, toggleMenu }) => {
             </Link>
           </div>
           <span className="lg:w-[1px] lg:min-w-[1px] w-full min-w-full h-[1px] lg:h-[unset] bg-[#4B4B4B]"></span>
-          <div className="flex flex-col lg:gap-y-[unset] sm:gap-y-[15px] gap-y-[10px] justify-start lg:text35 sm:mtext28 mtext22 lg:leading-[3.125vw] tracking-tight w-full lg:w-[40%]">
+          <div className="flex flex-col lg:gap-y-[unset] sm:gap-y-[15px] gap-y-[10px] justify-start lg:text35 sm:mtext28 mtext22 lg:leading-[3.125vw] tracking-tight w-full lg:w-[41%]">
             <Link
               onClick={toggleMenu}
               href={"/service-singlepage"}
