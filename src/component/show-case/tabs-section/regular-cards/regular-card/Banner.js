cbe260ef99b66a9a5ab7d992dcecaadc09828e8b
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2025-03-10 17:43:19 +0500

    origin

--- src/component/show-case/tabs-section/regular-cards/regular-card/Banner.js
+++ src/component/show-case/tabs-section/regular-cards/regular-card/Banner.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 const Banner = ({ images }) => {
+  console.log(images[images.length - 1]);
   return (
     <div className="aspect-video relative rounded-[8px] overflow-hidden w-full">
       {images.length > 0 && (
@@ -19,7 +20,7 @@ const Banner = ({ images }) => {
           fill
           alt="banner"
           className="object-cover opacity-0 group-hover:opacity-[1] transition-all duration-300  absolute top-0 left-0 w-full h-full"
-          src={images[1]}
+          src={images[images.length - 1]}
         />
       )}
     </div>
